6a222cdfad275cdf5b0d350fa793f130cba5eaf2
Simon Métais <metais.simon@icloud.com>
2024-06-13 16:16:49 +0200

    entrypoint

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,4 +6,4 @@ python manage.py migrate
 # collects all static files in our app and puts it in the STATIC_ROOT
 python manage.py collectstatic --noinput
 
-gunicorn django_tests.wsgi -b 0.0.0.0:8000
+gunicorn django_app.wsgi -b 0.0.0.0:8000
