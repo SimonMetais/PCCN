53f23902dad325edf4596176f58dd0f314d22e2a
Simon Métais <metais.simon@icloud.com>
2024-06-24 18:40:46 +0200

    footer v0

--- django_app/base/models/home.py
+++ django_app/base/models/home.py
@@ -1,11 +1,23 @@
 from django.db import models as m
-from django.urls import reverse
+
+horaires = """
+✅ Lun 10h-19h
+✅ Mar 10h-19h
+✅ Mer 10h-19h
+✅ Jeu 10h-19h
+✅ Ven 10h-19h
+❌ Sam férmé
+❌ Dim férmé
+"""
 
 
 class Home(m.Model):
     logo = m.ImageField(upload_to='Home/')
-    home_text = m.TextField(verbose_name="Texte d'accueil")
-    location = m.CharField(max_length=255, verbose_name="Adresse")
+    home_text = m.TextField(verbose_name="Texte d'accueil", default="Bienvenue !")
+    location = m.CharField(max_length=255, verbose_name="Adresse", default="6 rue des fleurs 17589 Queleque_part")
+    phone_number = m.CharField(max_length=20, verbose_name="Numéro de téléphone", default="+33 (0)6 11 22 33 44")
+    email = m.EmailField(verbose_name="e-mail", default="contact@exemple.com")
+    opening_hours = m.TextField(verbose_name="Horaire d'ouverture", default=horaires)
 
     class Meta:
         verbose_name = 'Accueil'
--- django_app/base/views/global_context.py
+++ django_app/base/views/global_context.py
@@ -2,7 +2,13 @@ from base.models import Home
 
 
 def global_data(request):
-    home_stuffs: Home = Home.objects.first()
-    return {
-        'location': home_stuffs.location,
+    header = {
+        'Protégés': 'animals',
+        'Publications': 'publications',
+        'Partenaires': 'sponsors',
     }
+    return {
+        'header_list': header.items(),
+        'home_stuffs': Home.objects.first(),
+    }
+
