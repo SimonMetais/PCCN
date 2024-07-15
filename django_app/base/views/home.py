8d28954785e1a6fb0c01dd742010e840be21ea04
Simon Métais <metais.simon@icloud.com>
2024-07-15 15:50:11 +0200

    section quick_link_cards ok

--- django_app/base/views/home.py
+++ django_app/base/views/home.py
@@ -5,7 +5,13 @@ from base.models import Home, Publication
 
 def home(request):
     home_stuffs: Home = Home.objects.first()
+    quick_link_cards = {
+        'donate': "<strong>Donnez</strong> pour eux",
+        'adopt': "<strong>Adoptez</strong> un protégé",
+        'volunteer': "Devenez <strong>volontaire</strong>",
+    }
     context = {
         'last_publication': Publication.objects.first(),
+        'q_cards': quick_link_cards.items(),
     }
     return render(request, 'home2.html', context=context)
