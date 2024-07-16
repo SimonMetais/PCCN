07d9343a56e5b41fb43b2ddf1f506689e545e3b6
Simon Métais <metais.simon@icloud.com>
2024-07-16 11:44:40 +0200

    save

--- django_app/base/views/home.py
+++ django_app/base/views/home.py
@@ -6,12 +6,12 @@ from base.models import Home, Publication
 def home(request):
     home_stuffs: Home = Home.objects.first()
     quick_link_cards = {
-        'donate': "<strong>Donnez</strong> pour eux",
-        'adopt': "<strong>Adoptez</strong> un protégé",
-        'volunteer': "Devenez <strong>volontaire</strong>",
+        'donate': ("<strong>Donnez</strong> pour eux", 'https://www.google.com/'),
+        'adopt': ("<strong>Adoptez</strong> un protégé", 'https://www.google.com/'),
+        'volunteer': ("Devenez <strong>volontaire</strong>", 'https://www.google.com/'),
     }
     context = {
         'last_publication': Publication.objects.first(),
         'q_cards': quick_link_cards.items(),
     }
-    return render(request, 'home2.html', context=context)
+    return render(request, 'home.html', context=context)
