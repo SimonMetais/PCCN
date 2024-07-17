b8d0521f399b48c9b0c61fc2d8756a83b9e0dbb5
Simon Métais <metais.simon@icloud.com>
2024-07-17 17:06:37 +0200

    publi v1 ok

---
+++ django_app/base/templatetags/filters.py
@@ -0,0 +1,22 @@
+import re
+
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.html import urlize, linebreaks
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+url_pattern = re.compile(r'[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)')
+
+
+@register.filter
+@stringfilter
+def publication_format(texte: str, max_lenght):
+    texte = linebreaks(texte)
+
+    words = texte.split()
+    if len(words) > max_lenght:
+        texte = ' '.join(words[:max_lenght]) + '...<u> Voir la suite</u>'
+
+    texte = url_pattern.sub(lambda x: urlize(x.group()), texte)
+    return mark_safe(texte)
--- django_app/base/views/home.py
+++ django_app/base/views/home.py
@@ -6,9 +6,9 @@ from base.models import Home, Publication
 def home(request):
     home_stuffs: Home = Home.objects.first()
     quick_link_cards = {
-        'donate': ("<strong>Donnez</strong> pour eux", 'https://www.google.com/'),
-        'adopt': ("<strong>Adoptez</strong> un protégé", 'https://www.google.com/'),
-        'volunteer': ("Devenez <strong>volontaire</strong>", 'https://www.google.com/'),
+        'donate': ("<strong>Donnez</strong> pour eux", 'https://www.helloasso.com/associations/pccn#highlight'),
+        'adopt': ("<strong>Adoptez</strong> un protégé", '#'),
+        'volunteer': ("Devenez <strong>volontaire</strong>", 'https://www.helloasso.com/associations/pccn/adhesions/adhesion-1'),
     }
     context = {
         'last_publication': Publication.objects.first(),
